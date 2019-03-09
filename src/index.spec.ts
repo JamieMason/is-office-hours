e61a25d09ebe1a7378470896cc49284e6adc4981
Jamie Mason <jamie@foldleft.io>
2019-03-09 17:56:05 +0000

    feat(core): assert if date is mon-fri 9am-5pm

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,47 @@
 import 'expect-more-jest';
 import { isOfficeHours } from '.';
 
-it('is a function', () => {
-  expect(isOfficeHours).not.toThrowError();
+const checkDate = (isoDate: string, isoTime: string) =>
+  isOfficeHours(new Date(`${isoDate}T${isoTime}.000Z`));
+
+[
+  ['Monday', '2019-03-04'],
+  ['Tuesday', '2019-03-05'],
+  ['Wednesday', '2019-03-06'],
+  ['Thursday', '2019-03-07'],
+  ['Friday', '2019-03-08']
+].forEach(([workingDay, isoDate]) => {
+  it(`Returns false until 9:00am  ${workingDay}`, () => {
+    expect(checkDate(isoDate, '08:59:59')).toBeFalse();
+  });
+
+  it(`Returns true between 9:00am and 4:59pm ${workingDay}`, () => {
+    expect(checkDate(isoDate, '09:00:00')).toBeTrue();
+    expect(checkDate(isoDate, '09:01:00')).toBeTrue();
+    expect(checkDate(isoDate, '16:59:59')).toBeTrue();
+  });
+
+  it(`Returns false from 5:00pm  ${workingDay}`, () => {
+    expect(checkDate(isoDate, '17:00:00')).toBeFalse();
+    expect(checkDate(isoDate, '17:01:00')).toBeFalse();
+  });
 });
+
+[['Saturday', '2019-03-09'], ['Sunday', '2019-03-10']].forEach(
+  ([restDay, isoDate]) => {
+    it(`Returns false until 9:00am  ${restDay}`, () => {
+      expect(checkDate(isoDate, '08:59:59')).toBeFalse();
+    });
+
+    it(`Returns false between 9:00am and 4:59pm ${restDay}`, () => {
+      expect(checkDate(isoDate, '09:00:00')).toBeFalse();
+      expect(checkDate(isoDate, '09:01:00')).toBeFalse();
+      expect(checkDate(isoDate, '16:59:59')).toBeFalse();
+    });
+
+    it(`Returns false from 5:00pm  ${restDay}`, () => {
+      expect(checkDate(isoDate, '17:00:00')).toBeFalse();
+      expect(checkDate(isoDate, '17:01:00')).toBeFalse();
+    });
+  }
+);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export const isOfficeHours = () => {
-  /*  */
-};
+const isWorkingDay = (date: Date) => date.getDay() >= 1 && date.getDay() <= 5;
+
+export const isOfficeHours = (date: Date): boolean =>
+  isWorkingDay(date) && date.getHours() >= 9 && date.getHours() < 17;
